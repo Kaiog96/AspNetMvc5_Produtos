4c6b974b98b07481efc75715334401b2ba02244c
Kaio Garcia <contatokaiogarcia@gmail.com>
2022-02-08 20:30:05 -0300

    ASP.NET MVC 5

---
+++ src/DevIO.AppMvc/Global.asax.cs
@@ -0,0 +1,13 @@
+
+using System.Web;
+
+namespace DevIO.AppMvc
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+           
+        }
+    }
+}
---
+++ src/DevIO.Business/Models/Produtos/IProdutoRepository.cs
@@ -0,0 +1,16 @@
+﻿using DevIO.Business.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Models.Produtos
+{
+    public interface IProdutoRepository : IRepository<Produto>
+    {
+        Task<IEnumerable<Produto>> ObterProdutosPorFornecedor(Guid fornecedorid);
+
+        Task<IEnumerable<Produto>> ObterProdutosFornecedores();
+
+        Task<Produto> ObterProdutoFornecedor(Guid id);
+    }
+}
